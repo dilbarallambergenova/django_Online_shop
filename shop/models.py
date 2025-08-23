fdaa042210661fb19e0075d8cbc3ad910a2ce69c
dilbarallambergenova <allambergenovadilbar9@gmail.com>
2025-08-23 09:46:42 +0500

    yangilandi

--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,6 @@ class Category(models.Model):
     name=models.CharField(max_length=100)
     icon = models.CharField(max_length=5, blank=True,null=True)
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
-
     def __str__(self):
         return self.name
 
